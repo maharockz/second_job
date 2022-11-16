d40c8ae1e52ab2f15adaf13e1609c6c6256080ef
Saitejavanumu1234 <saitejavanumu0@gmail.com>
2022-11-16 17:07:34 +0530

    changes by geetha

---
+++ myfile.java
@@ -0,0 +1,6 @@
+[A[B[B[A[Bclass A
+{
+public static void main(string[] args)
+{
+System.out.println("hello world");
+}
---
+++ sub.java
@@ -0,0 +1,10 @@
+public class sub
+{
+public static void main(string args[])
+{
+ int a=5,c;
+ int b=2;
+ c=a-b;
+  System.out.println("subraction"+c);
+}
+}
